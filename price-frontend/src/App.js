41fde954727eecdfbfa94fbff8f71fefbfa7f5c8
Benjamin Colasante <benjamin.colasante@hotmail.com>
2025-08-12 14:33:32 -0400

    Background

--- price-frontend/src/App.js
+++ price-frontend/src/App.js
@@ -6,11 +6,25 @@ import Home from './Home';
 import ResultPage from './ResultPage';
 import SavedProducts from './SavedProducts';
 
+
+
 function App() {
+  const appStyle = {
+    backgroundImage: "url('/background.jpg')",
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundAttachment: 'fixed',
+    minHeight: '100vh',
+    minWidth: '100vw',
+    width: '100%',
+    height: 'auto',
+    paddingTop: '60px',
+  };
+
   return (
     <BookmarkProvider>
       <Router>
-        <div className="App">
+        <div className="App" style={appStyle}>
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
