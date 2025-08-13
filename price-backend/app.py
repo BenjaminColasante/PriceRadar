74f65adb06b01efec6db9abf84698a03e867f20b
Benjamin Colasante <benjamin.colasante@hotmail.com>
2025-08-13 13:48:15 -0400

    Update app.py

--- price-backend/app.py
+++ price-backend/app.py
@@ -4,7 +4,7 @@ from walmart_scraper import fetch_walmart_items
 from amazon_scraper import fetch_amazon_items
 
 app = Flask(__name__)
-CORS(app, origins=["https://price-radar-p4enj1xyg-benjamins-projects-0d22a47c.vercel.app"])
+CORS(app, origins=["http://localhost:3000", "https://price-radar-khaki.vercel.app/"])
 
 def parse_bool(v,default=False):
     if v is None:
